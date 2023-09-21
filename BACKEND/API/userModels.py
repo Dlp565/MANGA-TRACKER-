08aed037e598685681626e90c9bdb7452ffc1ae6
Dileepa Manage <68203475+Dlp565@users.noreply.github.com>
2023-09-21 19:53:20 -0400

    Deleted some junk

--- BACKEND/API/userModels.py
+++ BACKEND/API/userModels.py
@@ -29,22 +29,3 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
     username: str | None = None
-
-
-"""
-class Test(BaseModel):
-    id: str = Field(default_factory=uuid.uuid4, alias="_id")
-    title: str = Field(...)
-    author: str = Field(...)
-    
-
-    class Config:
-        allow_population_by_field_name = True
-        schema_extra = {
-            "example": {
-                "_id": "066de609-b04a-4b30-b46c-32537c7f1f6e",
-                "title": "One Piece",
-                "author": "Eiichiro Oda"
-            }
-        }
-"""
